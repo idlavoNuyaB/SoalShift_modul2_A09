82ac6bed6d4a18262e4d8795441aded35194e1f8
idlavoNuyaB <36811359+idlavoNuyaB@users.noreply.github.com>
2019-03-15 07:50:27 +0700

    Edit comment

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -17,13 +17,13 @@ int main() {
   exit(EXIT_FAILURE);
   }
   if (pid > 0) {
-    char *buat[4] = {"mkdir","-p","modul2",NULL};
+    char *buat[4] = {"mkdir","-p","modul2",NULL};//folder modul2
     execv("/bin/mkdir",buat);
   }
   else {
     pid_1 =fork();
     if (pid_1 > 0){
-       char *buatlagi[4] = {"mkdir","-p","modul2/gambar",NULL};
+       char *buatlagi[4] = {"mkdir","-p","modul2/gambar",NULL};//folder gambar
        execv("/bin/mkdir",buatlagi);
     }
     else {
@@ -43,21 +43,21 @@ int main() {
     		struct dirent *de;
        		char *temp,*temp2,oldname[30],grey[10],png[10],direct[100],direct2[100];
        		DIR *dr = opendir("/home/arvanna/Pictures");
-       		if(dr==NULL)return 0;
-       		while((de=readdir(dr))!=NULL){
+       		if(dr==NULL)return 0;//jika tidak ada directorynya
+       		while((de=readdir(dr))!=NULL){//selama dibaca ada
            		strcpy(direct,"/home/arvanna/Pictures/");
            		strcpy(direct2,"/home/arvanna/modul2/gambar/");
-           		temp=strstr(de->d_name,".png");
+           		temp=strstr(de->d_name,".png");//ngecek jika sama dengan png
            		strcpy(png,".png");
            		strcpy(grey,"_grey.png");
            		if(temp){
-                		temp2=strtok(de->d_name,".");
-                		strcpy(oldname,temp2);
-						strcat(oldname,png);
-                		strcat(temp2,grey);
-                		strcat(direct,oldname);
-                		strcat(direct2,temp2);
-                		rename(direct,direct2);
+                		temp2=strtok(de->d_name,".");//ngesplit nama dengan png
+                		strcpy(oldname,temp2);//mengduplicate nama lama dengan nama baru 
+				strcat(oldname,png);//nama lama +.png
+                		strcat(temp2,grey);//nama baru +-grey.png
+                		strcat(direct,oldname);//gabung directory baru + nama baru
+                		strcat(direct2,temp2);//gabung directory lama + nama lama
+                		rename(direct,direct2);//memindahkan
                 		//printf("%s\n",oldname);
                 		//printf("%s\n",temp2);
                 		//char *cp[]={"cp",temp2,"/home/arvanna/modul2/gambar",NULL};
