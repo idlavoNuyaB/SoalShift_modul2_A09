4f9a9dbe3c8268ca67e1746955665f1037eb594a
idlavoNuyaB <36811359+idlavoNuyaB@users.noreply.github.com>
2019-03-15 07:54:30 +0700

    Edit Comment lagi wkkwkw

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -41,23 +41,23 @@ int main()
         struct dirent *de;
         DIR *dr=opendir("/home/arvanna/modul2/hatiku");
         char nama[10],dir[100]="/home/arvanna/modul2/hatiku/";
-        if(dr==NULL)exit(EXIT_FAILURE);
+        if(dr==NULL)exit(EXIT_FAILURE);//sama kayak no 1
         while((de=readdir(dr))!=NULL){
-          strcpy(nama,de->d_name);
-          if(strcmp(nama,"elen.ku")==0){
+          strcpy(nama,de->d_name);//mengcopy list file ke nama
+          if(strcmp(nama,"elen.ku")==0){//jika ada file yg namanya elen.ku
               struct stat info;
-	            strcat(dir,nama);
-              if(stat(dir,&info)==0){
+	      strcat(dir,nama);//menggabungkan directorynya dan elen.ku
+              if(stat(dir,&info)==0){//memasukkan informasi dari dir ke info
                   //printf("%d %d %d",info.st_uid,info.st_gid,info.st_mode);
                   //printf("%s",nama);
-                  char perm[]="0777";
-                  int i=strtol(perm,0,8);
-                  chmod(dir,i);
-                  struct passwd *owner=getpwuid(info.st_uid);
-                  struct group *grup=getgrgid(info.st_gid);
+                  char perm[]="0777";//masih basis 10
+                  int i=strtol(perm,0,8);//diubah ke basis 8
+                  chmod(dir,i);//mengubah permission ke 0777
+                  struct passwd *owner=getpwuid(info.st_uid);//memasukkan user id info ke struct owner
+                  struct group *grup=getgrgid(info.st_gid);//memasukkan group id info ke struct group
                   //printf("%s %s",owner->pw_name,grup->gr_name);
-                  if(strcmp(owner->pw_name,"www-data")==0 && strcmp(grup->gr_name,"www-data")==0){
-                        remove(dir);
+                  if(strcmp(owner->pw_name,"www-data")==0 && strcmp(grup->gr_name,"www-data")==0){//selama nama owner dan group owner www-data
+                        remove(dir);//di hapus
                       //printf("%s",nama);
                   }
                 }
